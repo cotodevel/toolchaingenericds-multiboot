f523426bad923b0fc1ee826c5b5fe05c905e37da
Coto <programmer.coto.1988@gmail.com>
2019-11-03 13:43:18 -0300

    add placeholders

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# toolchaingenericds-multiboot
+This is the Toolchain Generic multiboot project:
 
+Compile Toolchain: To compile this project you will need to follow the steps at https://bitbucket.org/Coto88/toolchaingenericds : Then simply extract the project somewhere.
+
+Compile this project: Open msys, through msys commands head to the directory your extracted this project. Then write: make clean make
+
+After compiling, run the example in NDS.
+
+Project Specific description: 
+
+WIP
+
+/release folder has the latest binary precompiled for your convenience.
+
+Coto
---
+++ arm9/source/main.cpp
@@ -0,0 +1,477 @@
+/*
+
+			Copyright (C) 2017  Coto
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+
+*/
+
+#include "dsregs.h"
+#include "dsregs_asm.h"
+#include "typedefsTGDS.h"
+
+#include "socket.h"
+#include "in.h"
+#include <netdb.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+//C++ part
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <iterator>
+
+using namespace std;
+
+#include "main.h"
+#include "InterruptsARMCores_h.h"
+#include "ipcfifoTGDSUser.h"
+#include "ff.h"
+#include "fileHandleTGDS.h"
+#include "reent.h"
+#include "sys/types.h"
+#include "consoleTGDS.h"
+
+#include "devoptab_devices.h"
+#include "fatfslayerTGDS.h"
+#include "usrsettingsTGDS.h"
+#include "exceptionTGDS.h"
+
+#include "videoTGDS.h"
+#include "keypadTGDS.h"
+#include "dldi.h"
+#include "SpecialFunctions.h"
+#include "dmaTGDS.h"
+#include "biosTGDS.h"
+#include "nds_cp15_misc.h"
+#include "limitsTGDS.h"
+#include "dswnifi_lib.h"
+#include "utilsTGDS.h"
+
+char curChosenBrowseFile[MAX_TGDSFILENAME_LENGTH+1];
+
+string ToStr( char c ) {
+   return string( 1, c );
+}
+
+std::string getDldiDefaultPath(){
+	std::string dldiOut = string((char*)getfatfsPath( (sint8*)string(dldi_tryingInterface() + string(".dldi")).c_str() ));
+	return dldiOut;
+}
+
+void menuShow(){
+	clrscr();
+	printf("                              ");
+	printf("Y: Read File: 0:/filelist.txt");
+	printf("X: generate root file list into 0:/filelist.txt");
+	printf("L: dump dldi file to %s",getDldiDefaultPath().c_str());
+	printf("Start: simple file browser");
+	printf("Select: this menu");
+}
+
+//customHandler 
+void CustomDebugHandler(){
+	clrscr();
+	
+	//Init default console here
+	bool project_specific_console = false;	//set default console or custom console: default console
+	GUI_init(project_specific_console);
+	GUI_clear();
+	
+	printf("custom Handler!");
+	uint32 * debugVector = (uint32 *)&exceptionArmRegs[0];
+	uint32 pc_abort = (uint32)exceptionArmRegs[0xf];
+	
+	if((debugVector[0xe] & 0x1f) == 0x17){
+		pc_abort = pc_abort - 8;
+	}
+	
+	printf("R0[%x] R1[%X] R2[%X] \n",debugVector[0],debugVector[1],debugVector[2]);
+	printf("R3[%x] R4[%X] R5[%X] \n",debugVector[3],debugVector[4],debugVector[5]);
+	printf("R6[%x] R7[%X] R8[%X] \n",debugVector[6],debugVector[7],debugVector[8]);
+	printf("R9[%x] R10[%X] R11[%X] \n",debugVector[9],debugVector[0xa],debugVector[0xb]);
+	printf("R12[%x] R13[%X] R14[%X]  \n",debugVector[0xc],debugVector[0xd],debugVector[0xe]);
+	printf("R15[%x] SPSR[%x] CPSR[%X]  \n",pc_abort,debugVector[17],debugVector[16]);
+	while(1==1){}
+}
+
+vector<string> splitCustom(string str, string token){
+    vector<string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index != (int)string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
+
+
+std::string parseDirNameTGDS(std::string dirName){
+	if ((dirName.at(0) == '/') && (dirName.at(1) == '/')) {
+		dirName.erase(0,1);	//trim the starting / if it has one
+	}
+	dirName.erase(dirName.length());	//trim the leading "/"
+	return dirName;
+}
+
+std::string parsefileNameTGDS(std::string fileName){
+	if ((fileName.at(2) == '/') && (fileName.at(3) == '/')) {
+		fileName.erase(2,2);	//trim the starting // if it has one (since getfspath appends 0:/)
+		if(fileName.at(2) != '/'){	//if we trimmed by accident the only leading / such as 0:filename instead of 0:/filename, restore it so it becomes the latter
+			fileName.insert(2, ToStr('/') );
+		}
+	}
+	return fileName;
+}
+
+bool ShowBrowser(char * Path){
+	while((keysPressed() & KEY_START) || (keysPressed() & KEY_A) || (keysPressed() & KEY_B)){
+		scanKeys();
+	}
+	int pressed = 0;
+	vector<struct FileClass *> internalName;
+	struct FileClass filStub;
+	char fname[256];
+	sprintf(fname,"%s",Path);
+	int j = 1;
+    
+	//OK, use the new CWD and build the playlist
+	internalName.push_back(&filStub);
+	
+	int retf = FAT_FindFirstFile(fname);
+	while(retf != FT_NONE){
+		struct FileClass * fileClassInst = NULL;
+		//directory?
+		if(retf == FT_DIR){
+			fileClassInst = getFileClassFromList(LastDirEntry);
+			std::string outDirName = string(fileClassInst->fd_namefullPath);
+			sprintf(fileClassInst->fd_namefullPath,"%s",parseDirNameTGDS(outDirName).c_str());
+		}
+		//file?
+		else if(retf == FT_FILE){
+			fileClassInst = getFileClassFromList(LastFileEntry); 
+			std::string outFileName = string(fileClassInst->fd_namefullPath);
+			
+			sprintf(fileClassInst->fd_namefullPath,"%s",parsefileNameTGDS(outFileName).c_str());
+		}
+		internalName.push_back(fileClassInst);
+		
+		//more file/dir objects?
+		retf = FAT_FindNextFile(fname);
+	}
+	
+	//actual file lister
+	clrscr();
+	
+	j = 1;
+	pressed = 0 ;
+	int lastVal = 0;
+	bool reloadDirA = false;
+	bool reloadDirB = false;
+	std::string newDir = std::string("");
+	
+	#define itemsShown (int)(15)
+	int curjoffset = 0;
+	int itemRead=1;
+	
+	while(1){
+		
+		int itemsToLoad = (internalName.size() - curjoffset);
+		
+		//check if remaining items are enough
+		if(itemsToLoad > itemsShown){
+			itemsToLoad = itemsShown;
+		}
+		
+		while(itemRead < itemsToLoad ){
+			std::string strDirFileName = string(internalName.at(itemRead+curjoffset)->fd_namefullPath);		
+			if(internalName.at(itemRead+curjoffset)->type == FT_DIR){
+				printfCoords(0, itemRead, "--- %s%s",strDirFileName.c_str(),"<dir>");
+			}
+			else{
+				printfCoords(0, itemRead, "--- %s",strDirFileName.c_str());
+			}
+			itemRead++;
+		}
+		
+		scanKeys();
+		pressed = keysPressed();
+		if (pressed&KEY_DOWN && (j < (itemsToLoad - 1) ) ){
+			j++;
+			while(pressed&KEY_DOWN){
+				scanKeys();
+				pressed = keysPressed();
+			}
+		}
+		
+		//downwards: means we need to reload new screen
+		else if(pressed&KEY_DOWN && (j >= (itemsToLoad - 1) ) && ((internalName.size() - curjoffset - itemRead) > 0) ){
+			
+			//list only the remaining items
+			clrscr();
+			
+			curjoffset = (curjoffset + itemsToLoad - 1);
+			itemRead = 1;
+			j = 1;
+			
+			scanKeys();
+			pressed = keysPressed();
+			while(pressed&KEY_DOWN){
+				scanKeys();
+				pressed = keysPressed();
+			}
+		}
+		
+		if (pressed&KEY_UP && (j > 1)) {
+			j--;
+			while(pressed&KEY_UP){
+				scanKeys();
+				pressed = keysPressed();
+			}
+		}
+		
+		//upwards: means we need to reload new screen
+		else if (pressed&KEY_UP && (j <= 1) && (curjoffset > 0) ) {
+			//list only the remaining items
+			clrscr();
+			
+			curjoffset--;
+			itemRead = 1;
+			j = 1;
+			
+			scanKeys();
+			pressed = keysPressed();
+			while(pressed&KEY_UP){
+				scanKeys();
+				pressed = keysPressed();
+			}
+		}
+		
+		
+		//reload DIR (forward)
+		if( (pressed&KEY_A) && (internalName.at(j+curjoffset)->type == FT_DIR) ){
+			newDir = string(internalName.at(j+curjoffset)->fd_namefullPath);
+			reloadDirA = true;
+			break;
+		}
+		
+		//file chosen
+		else if( (pressed&KEY_A) && (internalName.at(j+curjoffset)->type == FT_FILE) ){
+			break;
+		}
+		
+		//reload DIR (backward)
+		else if(pressed&KEY_B){
+			reloadDirB = true;
+			break;
+		}
+		
+		// Show cursor
+		printfCoords(0, j, "*");
+		if(lastVal != j){
+			printfCoords(0, lastVal, " ");	//clean old
+		}
+		lastVal = j;
+	}
+	
+	//enter a dir
+	if(reloadDirA == true){
+		internalName.clear();
+		enterDir((char*)newDir.c_str());
+		return true;
+	}
+	
+	//leave a dir
+	if(reloadDirB == true){
+		//rewind to preceding dir in TGDSCurrentWorkingDirectory
+		leaveDir(getTGDSCurrentWorkingDirectory());
+		return true;
+	}
+	
+	sprintf((char*)curChosenBrowseFile,"%s",internalName.at(j+curjoffset)->fd_namefullPath);
+	clrscr();
+	printf("                                   ");
+	
+	if(internalName.at(j+curjoffset)->type == FT_DIR){
+		//printf("you chose Dir:%s",curChosenBrowseFile);
+	}
+	else if(internalName.at(j+curjoffset)->type == FT_FILE){
+		printf("you chose File:%s",curChosenBrowseFile);
+	}
+	return false;
+}
+
+int main(int _argc, sint8 **_argv) {
+	
+	/*			TGDS 1.5 Standard ARM9 Init code start	*/
+	bool project_specific_console = false;	//set default console or custom console: default console
+	GUI_init(project_specific_console);
+	GUI_clear();
+	
+	sint32 fwlanguage = (sint32)getLanguage();
+	
+	printf("     ");
+	printf("     ");
+	
+	int ret=FS_init();
+	if (ret == 0)
+	{
+		printf("FS Init ok.");
+	}
+	else if(ret == -1)
+	{
+		printf("FS Init error.");
+	}
+	switch_dswnifi_mode(dswifi_idlemode);
+	/*			TGDS 1.5 Standard ARM9 Init code end	*/
+	
+	//custom Handler
+	setupCustomExceptionHandler((uint32*)&CustomDebugHandler);
+	menuShow();
+	
+	while (1){
+		scanKeys();
+		
+		if (keysPressed() & KEY_START){
+			
+			//as long you keep using directories ShowBrowser will be true
+			char startPath[MAX_TGDSFILENAME_LENGTH+1];
+			sprintf(startPath,"%s","/");
+			while( ShowBrowser((char *)startPath) == true ){
+				
+			}
+			while(keysPressed() & KEY_START){
+				scanKeys();
+			}
+			menuShow();
+		}
+		
+		if (keysPressed() & KEY_Y){
+		
+			char InFile[80];  // input file name
+			char ch;
+			
+			ifstream InStream;
+			std::string someString;
+			
+			ofstream OutStream;
+			
+			
+			sprintf(InFile,"%s%s",getfatfsPath((char*)""),"filelist.txt");
+			
+			// Open file for input
+			// in.open(fin); also works
+			InStream.open(InFile, ios::in);
+
+			// ensure file is opened successfully
+			if(!InStream)
+			{
+				printf("Error open file %s",InFile);
+				//return 1;
+			}
+			else{
+				printf("OK open file %s",InFile);
+				
+				// Read in each character until eof character is read.
+				// Output it to screen.
+				int somePosition = 0;
+				while (!InStream.eof()) {
+					//Read each character.
+					InStream.get(ch);
+					someString.insert(somePosition, ToStr(ch));
+					somePosition++;
+				}
+				InStream.close();
+				printf("->%s",someString.c_str());
+			}
+			
+			while(keysPressed() & KEY_Y){
+				scanKeys();
+			}
+		}
+		
+		if (keysPressed() & KEY_SELECT){
+			menuShow();
+		}
+		
+		if (keysPressed() & KEY_X){
+			
+			string fOut = string(getfatfsPath((char *)"filelist.txt"));
+			std::ofstream outfile;
+			outfile.open(fOut.c_str());
+			char fname[MAX_TGDSFILENAME_LENGTH+1] = {0};
+			int retf = FAT_FindFirstFile(fname);
+			while(retf != FT_NONE){
+				std::string fnameOut = std::string("");
+				//directory?
+				if(retf == FT_DIR){
+					struct FileClass * fileClassInst = getFileClassFromList(LastDirEntry);
+					std::string outDirName = string(fileClassInst->fd_namefullPath);
+					fnameOut = parseDirNameTGDS(outDirName) + string("/<dir>");
+				}
+				//file?
+				else if(retf == FT_FILE){
+					std::string outfileName = string(fname);
+					fnameOut = parsefileNameTGDS(outfileName);
+				}
+				outfile << fnameOut << endl;
+				
+				//more file/dir objects?
+				retf = FAT_FindNextFile(fname);
+			}
+			
+			outfile.close();
+			printf("filelist %s saved.", fOut.c_str());
+			while(keysPressed() & KEY_X){
+				scanKeys();
+			}
+		}
+		
+		if (keysPressed() & KEY_L){
+			DLDI_INTERFACE* dldiInterface = dldiGet();
+			uint8 * dldiStart = (uint8 *)dldiInterface;
+			int dldiSize = (int)pow((double)2, (double)dldiInterface->driverSize);
+			FILE * fh = fopen(getDldiDefaultPath().c_str(),"w+");
+			if(fh){
+				fwrite(dldiStart, 1, dldiSize, fh);
+				fclose(fh);
+				printf("%s exported.",getDldiDefaultPath().c_str());
+				printf("%d bytes",dldiSize);
+			}
+			while(keysPressed() & KEY_L){
+				scanKeys();
+			}
+		}
+		
+		IRQVBlankWait();
+	}
+
+}
