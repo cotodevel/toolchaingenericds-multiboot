ed23f57ba81336f2c6c4c6d44b120bf4191e35d6
Coto <programmer.coto.1988@gmail.com>
2021-02-20 21:52:04 -0300

    tgds-payload no longer embedded into binary. Default read method is: call to bootstub + read from filesystem.

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -31,7 +31,6 @@ USA
 #include "dldi.h"
 #include "loader.h"
 #include "dmaTGDS.h"
-#include "tgds_multiboot_payload.h"	//Portable TGDS-Multiboot payload here
 #include "nds_cp15_misc.h"
 #include "fileBrowse.h"
 #include <stdio.h>
@@ -75,26 +74,29 @@ void menuShow(){
 }
 
 //NTR Bootcode:
-void TGDSMultibootRunNDSPayload(char * filename) __attribute__ ((optnone)) {
+__attribute__((section(".itcm")))
+void TGDSMultibootRunNDSPayload(char * filename) __attribute__ ((optnone)) __attribute__ ((optnone)) {
 	strcpy((char*)(0x02280000 - (MAX_TGDSFILENAME_LENGTH+1)), filename);	//Arg0:	
-	coherent_user_range_by_size((uint32)&tgds_multiboot_payload[0], (int)tgds_multiboot_payload_size);
-	dmaTransferHalfWord(0, (uint32)&tgds_multiboot_payload[0], (u32)0x02280000, (uint32)tgds_multiboot_payload_size);
-	int ret=FS_deinit();
-	//Copy and relocate current TGDS DLDI section into target ARM9 binary
-	coherent_user_range_by_size((uint32)tgds_multiboot_payload_size, (int)tgds_multiboot_payload_size);
 	
-	bool stat = dldiPatchLoader((data_t *)0x02280000, (u32)tgds_multiboot_payload_size, (u32)&_io_dldi_stub);
-	if(stat == false){
-		//printf("DLDI Patch failed. APP does not support DLDI format.");
+	FILE * tgdsPayloadFh = fopen("0:/tgds_multiboot_payload.bin", "r");
+	if(tgdsPayloadFh != NULL){
+		fseek(tgdsPayloadFh, 0, SEEK_SET);
+		int	tgds_multiboot_payload_size = FS_getFileSizeFromOpenHandle(tgdsPayloadFh);
+		fread((u32*)0x02280000, 1, tgds_multiboot_payload_size, tgdsPayloadFh);
+		coherent_user_range_by_size(0x02280000, (int)tgds_multiboot_payload_size);
+		fclose(tgdsPayloadFh);
+		int ret=FS_deinit();
+		//Copy and relocate current TGDS DLDI section into target ARM9 binary
+		bool stat = dldiPatchLoader((data_t *)0x02280000, (u32)tgds_multiboot_payload_size, (u32)&_io_dldi_stub);
+		if(stat == false){
+			//printf("DLDI Patch failed. APP does not support DLDI format.");
+		}
+		REG_IME = 0;
+		typedef void (*t_bootAddr)();
+		t_bootAddr bootARM9Payload = (t_bootAddr)0x02280000;
+		bootARM9Payload();
 	}
-	
-	REG_IME = 0;
-	typedef void (*t_bootAddr)();
-	t_bootAddr bootARM9Payload = (t_bootAddr)0x02280000;
-	bootARM9Payload();
 }
-
-
 
 bool stopSoundStreamUser(){
 	return false;
