1bf83316259f4a0b6cb85d601cb3e13a81653a43
Coto <programmer.coto.1988@gmail.com>
2021-03-14 01:18:42 +0000

    - Correct unfreed memory by xmalloc when reloading a TGDS Binary.

--- release/arm9dldi-ntr/tgds_multiboot_payload.h
+++ release/arm9dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[848612];
+	extern unsigned char tgds_multiboot_payload[849252];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
