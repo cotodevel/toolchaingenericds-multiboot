9e890da58a2e1db390d2c0b3d4f1e60ae3631c9a
Coto <programmer.coto.1988@gmail.com>
2021-03-29 17:18:20 -0300

    emitting TGDS DSi (TWL) binaries!

---
+++ twl/stage2_9/arm9/source/interrupts/Interrupts_app9.c
@@ -0,0 +1,179 @@
+/*
+
+			Copyright (C) 2017  Coto
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+
+*/
+
+#include "InterruptsARMCores_h.h"
+#include "ipcfifoTGDSUser.h"
+#include "dsregs_asm.h"
+#include "main.h"
+#include "keypadTGDS.h"
+#include "interrupts.h"
+#include "utilsTGDS.h"
+#include "spifwTGDS.h"
+#include "videoGL.h"
+#include "videoTGDS.h"
+#include "math.h"
+
+//User Handler Definitions
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void IpcSynchandlerUser(uint8 ipcByte){
+	switch(ipcByte){
+		default:{
+			//ipcByte should be the byte you sent from external ARM Core through sendByteIPC(ipcByte);
+		}
+		break;
+	}
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void Timer0handlerUser(){
+
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void Timer1handlerUser(){
+
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void Timer2handlerUser(){
+
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void Timer3handlerUser(){
+
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void HblankUser(){
+
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void VblankUser(){
+	struct sIPCSharedTGDSSpecific* TGDSUSERIPC = getsIPCSharedTGDSSpecific();
+	if(TGDSUSERIPC->frameCounter9 < 60){
+		TGDSUSERIPC->frameCounter9++;
+	}
+	else{
+		TGDSUSERIPC->frameCounter9 = 0;
+	}
+}
+
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void VcounterUser(){
+	//any floating point gl call is being converted to fixed prior to being implemented
+	gluPerspective(30, 256.0 / 192.0, 0.1, 70);
+
+	gluLookAt(	0.0, 0.0, camDist,		//camera possition 
+				0.0, 0.0, 0.0,		//look at
+				0.0, 1.0, 0.0);		//up
+	
+	glLight(0, RGB15(31,31,31) , 0,				  floatov10(-1.0),		 0);
+	glLight(1, RGB15(31,0,31),   0,				  floatov10(1) - 1,			 0);
+	glLight(2, RGB15(0,31,0) ,   floatov10(-1.0), 0,					 0);
+	glLight(3, RGB15(0,0,31) ,   floatov10(1.0) - 1,  0,					 0);
+
+	glPushMatrix();
+
+	//move it away from the camera
+	glTranslate3f32(0, 0, floatof32(-1));
+			
+	glRotateX(rotateX);
+	glRotateY(rotateY);
+	
+	glMatrixMode(GL_TEXTURE);
+	glIdentity();
+	
+	glMatrixMode(GL_MODELVIEW);
+
+	glMaterialf(GL_AMBIENT, RGB15(8,8,8));
+	glMaterialf(GL_DIFFUSE, RGB15(16,16,16));
+	glMaterialf(GL_SPECULAR, BIT(15) | RGB15(8,8,8));
+	glMaterialf(GL_EMISSION, RGB15(5,5,5));
+
+	//ds uses a table for shinyness..this generates a half-ass one
+	glMaterialShinnyness();
+
+	//not a real gl function and will likely change
+	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FORMAT_LIGHT0 | POLY_FORMAT_LIGHT1 | 
+												POLY_FORMAT_LIGHT2 | POLY_FORMAT_LIGHT3 ) ;
+	
+	u32 keypad = keysHeld();
+	
+	/*
+	if(keypad & KEY_UP) rotateX += 3;
+	if(keypad & KEY_DOWN) rotateX -= 3;
+	if(keypad & KEY_LEFT) rotateY += 3;
+	if(keypad & KEY_RIGHT) rotateY -= 3;
+	
+	if(keypad & KEY_A) camDist += 0.3;
+	if(keypad & KEY_B) camDist -= 0.3;
+	*/
+	
+	glBindTexture(0, textureID);
+
+	//draw the obj
+	glBegin(GL_QUAD);
+		for(int i = 0; i < 6; i++){
+			drawQuad(i);
+		}
+	glEnd();
+	
+	glPopMatrix(1);
+		
+	glFlush();
+ 	
+	rotateX += 0.3;
+	rotateY += 0.3;
+}
+
+//Note: this event is hardware triggered from ARM7, on ARM9 a signal is raised through the FIFO hardware
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void screenLidHasOpenedhandlerUser(){
+
+}
+
+//Note: this event is hardware triggered from ARM7, on ARM9 a signal is raised through the FIFO hardware
+#ifdef ARM9
+__attribute__((section(".itcm")))
+#endif
+void screenLidHasClosedhandlerUser(){
+	
+}
---
+++ twl/tgds_multiboot_payload/source/main.c
@@ -0,0 +1,206 @@
+/*
+			Copyright (C) 2017  Coto
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+*/
+
+#include "main.h"
+#include "dsregs.h"
+#include "dsregs_asm.h"
+#include "typedefsTGDS.h"
+#include "gui_console_connector.h"
+#include "dswnifi_lib.h"
+#include "TGDSLogoLZSSCompressed.h"
+#include "ipcfifoTGDSUser.h"
+#include "fatfslayerTGDS.h"
+#include "cartHeader.h"
+#include "ff.h"
+#include "dldi.h"
+#include "loader.h"
+#include "dmaTGDS.h"
+#include "arm7bootldr.h"
+#include "nds_cp15_misc.h"
+#include "fileBrowse.h"
+#include <stdio.h>
+#include "biosTGDS.h"
+#include "global_settings.h"
+#include "posixHandleTGDS.h"
+#include "TGDSMemoryAllocator.h"
+#include "stage2_9.h"
+
+bool stopSoundStreamUser(){
+	return false;
+}
+
+void closeSoundUser(){
+	//Stubbed. Gets called when closing an audiostream of a custom audio decoder
+}
+
+//generates a table of sectors out of a given file. It has the ARM7 binary and ARM9 binary
+__attribute__((section(".itcm")))
+bool ReloadNDSBinaryFromContext(char * filename) __attribute__ ((optnone)) {
+	
+	//Will reload stage 2 payload and while passing it the filename into ARGV[0] vector
+	volatile u8 * outBuf7 = NULL;
+	volatile u8 * outBuf9 = NULL;
+
+	//copy loader code (arm7bootldr.bin) to ARM7's EWRAM portion while preventing Cache issues
+	coherent_user_range_by_size((uint32)&arm7bootldr[0], (int)arm7bootldr_size);					
+	memcpy ((void *)NDS_LOADER_IPC_BOOTSTUBARM7_CACHED, (u32*)&arm7bootldr[0], arm7bootldr_size); 	//memcpy ( void * destination, const void * source, size_t num );	//memset(void *str, int c, size_t n)
+	
+	int headerSize = sizeof(struct sDSCARTHEADER);
+	u8 * NDSHeader = (u8 *)TGDSARM9Malloc(headerSize*sizeof(u8));
+	memcpy(NDSHeader, (u8*)&stage2_9[0], headerSize);
+	printf("header parsed correctly.");
+	struct sDSCARTHEADER * NDSHdr = (struct sDSCARTHEADER *)NDSHeader;
+	
+	//fill data into shared buffers
+	int sectorSize = 0;
+	if(FF_MAX_SS == FF_MIN_SS){
+		sectorSize = (int)FF_MIN_SS;
+	}
+	else{
+		#if (FF_MAX_SS != FF_MIN_SS)
+		sectorSize = dldiFs.ssize;	//when dldiFs sectorsize is variable, it's 512 bytes per sector
+		#endif
+	}
+	
+	//Common
+	int clusterSizeBytes = getDiskClusterSizeBytes();
+	int sectorsPerCluster = dldiFs.csize;
+	NDS_LOADER_IPC_CTX_UNCACHED->sectorsPerCluster = sectorsPerCluster;
+	NDS_LOADER_IPC_CTX_UNCACHED->sectorSize = sectorSize;
+	
+	//ARM7
+	int arm7BootCodeSize = NDSHdr->arm7size;
+	u32 arm7BootCodeOffsetInFile = NDSHdr->arm7romoffset;
+	NDS_LOADER_IPC_CTX_UNCACHED->arm7EntryAddress = NDSHdr->arm7entryaddress;
+	NDS_LOADER_IPC_CTX_UNCACHED->bootCode7FileSize = arm7BootCodeSize;
+	int sectorOffsetStart7 = arm7BootCodeOffsetInFile % sectorSize;
+	int sectorOffsetEnd7 = (arm7BootCodeOffsetInFile + arm7BootCodeSize - sectorOffsetStart7) % sectorSize;
+	NDS_LOADER_IPC_CTX_UNCACHED->sectorOffsetStart7 = sectorOffsetStart7;
+	NDS_LOADER_IPC_CTX_UNCACHED->sectorOffsetEnd7 = sectorOffsetEnd7;
+	
+	//ARM9
+	int arm9BootCodeSize = NDSHdr->arm9size;
+	u32 arm9BootCodeOffsetInFile = NDSHdr->arm9romoffset;
+	NDS_LOADER_IPC_CTX_UNCACHED->arm9EntryAddress = NDSHdr->arm9entryaddress;
+	NDS_LOADER_IPC_CTX_UNCACHED->bootCode9FileSize = arm9BootCodeSize;
+	int sectorOffsetStart9 = arm9BootCodeOffsetInFile % sectorSize;
+	int sectorOffsetEnd9 = (arm9BootCodeOffsetInFile + arm9BootCodeSize - sectorOffsetStart9) % sectorSize;
+	NDS_LOADER_IPC_CTX_UNCACHED->sectorOffsetStart9 = sectorOffsetStart9;
+	NDS_LOADER_IPC_CTX_UNCACHED->sectorOffsetEnd9 = sectorOffsetEnd9;
+	
+	clrscr();
+	printf("-");
+	printf("-");
+	
+	//reset FP to make it sector-relative + get file size
+	int fileSize = stage2_9_size;
+	NDS_LOADER_IPC_CTX_UNCACHED->fileSize = fileSize;
+	
+	printf("ReloadNDSBinaryFromContext1():");
+	printf("arm7BootCodeSize:%d", arm7BootCodeSize);
+	printf("arm7BootCodeOffsetInFile:%x", arm7BootCodeOffsetInFile);
+	printf("arm7BootCodeEntryAddress:%x", NDS_LOADER_IPC_CTX_UNCACHED->arm7EntryAddress);
+	printf("arm9BootCodeSize:%d", arm9BootCodeSize);
+	printf("arm9BootCodeOffsetInFile:%x", arm9BootCodeOffsetInFile);
+	printf("arm9BootCodeEntryAddress:%x", NDS_LOADER_IPC_CTX_UNCACHED->arm9EntryAddress);
+	
+	printf("NDSLoader start. ");
+	
+	u8 * outBuf = (u8 *)TGDSARM9Malloc(sectorSize * sectorsPerCluster);
+	
+	//Uncached to prevent cache issues right at once
+	outBuf7 = (u8 *)(NDS_LOADER_IPC_ARM7BIN_UNCACHED);	//will not be higher than: arm7BootCodeSize
+	outBuf9 = (u8 *)(NDS_LOADER_IPC_CTX_UNCACHED->arm9EntryAddress | 0x400000); //will not be higher than: arm9BootCodeSize or 0x2D0000 (2,949,120 bytes)
+	
+	memcpy(outBuf7, (u8*)&stage2_9[arm7BootCodeOffsetInFile], arm7BootCodeSize);
+	memcpy(outBuf9, (u8*)&stage2_9[arm9BootCodeOffsetInFile], arm9BootCodeSize);
+	
+	//Build NDS Header
+	memcpy((u8*)0x027FFE00, NDSHeader, (headerSize*sizeof(u8)));
+	
+	printf("NDSLoader end. ");
+	
+	TGDSARM9Free(outBuf);
+	TGDSARM9Free(NDSHeader);
+	
+	//Copy and relocate current TGDS DLDI section into target ARM9 binary
+	//printf("Boot Stage2");
+	bool stat = dldiPatchLoader((data_t *)NDS_LOADER_IPC_CTX_UNCACHED->arm9EntryAddress, (u32)arm9BootCodeSize, (u32)&_io_dldi_stub);
+	if(stat == false){
+		printf("DLDI Patch failed. APP does not support DLDI format.");
+	}
+	
+	int ret=FS_deinit();
+	
+	asm("mcr	p15, 0, r0, c7, c10, 4");
+	WRAM_CR = WRAM_0KARM9_32KARM7;	//96K ARM7 : 0x037f8000 ~ 0x03810000
+	
+	runBootstrapARM7();	//ARM9 Side						/	
+	setNDSLoaderInitStatus(NDSLOADER_LOAD_OK);	//		|	Wait until ARM7.bin is copied back to IWRAM's target address
+	//waitWhileNotSetStatus(NDSLOADER_START);		//		\
+	
+	u32 arm9Addr = (uint32)NDS_LOADER_IPC_CTX_UNCACHED->arm9EntryAddress;
+	coherent_user_range_by_size(arm9Addr, (u32)arm9BootCodeSize);
+	memset(0x023C0000, 0, 0x4000);
+	reloadARMCore(arm9Addr);
+	
+	return false;
+}
+
+//ToolchainGenericDS-LinkedModule User implementation: Called if TGDS-LinkedModule fails to reload ARM9.bin from DLDI.
+char args[8][MAX_TGDSFILENAME_LENGTH];
+char *argvs[8];
+int TGDSProjectReturnFromLinkedModule() __attribute__ ((optnone)) {
+	return -1;
+}
+
+//This payload has all the ARM9 core hardware, TGDS Services, so SWI/SVC can work here.
+int main(int argc, char **argv)  __attribute__ ((optnone)) {
+	
+	/*			TGDS 1.6 Standard ARM9 Init code start	*/
+	bool isTGDSCustomConsole = false;	//set default console or custom console: default console
+	GUI_init(isTGDSCustomConsole);
+	GUI_clear();
+	
+	bool isCustomTGDSMalloc = true;
+	setTGDSMemoryAllocator(getProjectSpecificMemoryAllocatorSetup(TGDS_ARM7_MALLOCSTART, TGDS_ARM7_MALLOCSIZE, isCustomTGDSMalloc));
+	sint32 fwlanguage = (sint32)getLanguage();
+	
+	printf(" -- ");
+	printf(" -- ");
+	printf(" -- ");
+	printf(" -- ");
+	
+	int ret=FS_init();
+	if (ret == 0)
+	{
+		printf("FS Init ok.");
+	}
+	else if(ret == -1)
+	{
+		printf("FS Init error.");
+	}
+	asm("mcr	p15, 0, r0, c7, c10, 4");
+	flush_icache_all();
+	flush_dcache_all();
+	/*			TGDS 1.6 Standard ARM9 Init code end	*/
+	
+	
+	ReloadNDSBinaryFromContext(argv[0]);	//Boot NDS file
+	return 0;
+}
