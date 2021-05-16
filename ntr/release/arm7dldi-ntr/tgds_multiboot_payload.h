c9258d82967e251cd0ce66ccfc5be0c9b9775e08
Coto <programmer.coto.1988@gmail.com>
2021-05-16 05:47:46 +0100

    - Update TGDS Project to 1.63.1

--- ntr/release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ ntr/release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[946216];
+	extern unsigned char tgds_multiboot_payload[948944];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
--- twl/release/arm7dldi-twl/tgds_multiboot_payload.h
+++ twl/release/arm7dldi-twl/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[841704];
+	extern unsigned char tgds_multiboot_payload[844484];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
