2671c9e1f937397192ab46f2c20b62ca6799481d
Coto <programmer.coto.1988@gmail.com>
2021-05-18 16:10:46 +0100

    - TGDS NTR binaries preserve smaller binary size while TGDS TWL binaries implements TWL + NTR (linkage) code: / - generate librarian tgds 7 & tgds 7i / - generate librarian tgds 9 & tgds 9i

--- twl/release/arm7dldi-twl/tgds_multiboot_payload.h
+++ twl/release/arm7dldi-twl/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[844484];
+	extern unsigned char tgds_multiboot_payload[842492];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
