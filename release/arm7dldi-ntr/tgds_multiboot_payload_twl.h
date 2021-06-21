0d7115c076f9a8cf37e873fb26bd55c7db2bc2ed
Coto <programmer.coto.1988@gmail.com>
2021-06-20 23:32:21 -0400

    generate better twl payloads. Still, whitescreens on real TWL hardware, but works in NO$GBA

---
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -0,0 +1,8 @@
+#ifdef __cplusplus 
+extern "C" {
+#endif
+	extern unsigned char tgds_multiboot_payload_twl[986384];
+	extern int tgds_multiboot_payload_twl_size;
+#ifdef __cplusplus 
+} 
+#endif 
--- release/tgds_multiboot_payload_twl.h
+++
@@ -1,8 +0,0 @@
-#ifdef __cplusplus 
-extern "C" {
-#endif
-	extern unsigned char tgds_multiboot_payload_twl[973072];
-	extern int tgds_multiboot_payload_twl_size;
-#ifdef __cplusplus 
-} 
-#endif 
