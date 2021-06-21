ed4abed7921ce863409951edab49551b8c4740ff
Coto <programmer.coto.1988@gmail.com>
2021-06-21 11:30:16 -0400

    - Fix TGDS ARM7 TWL ARM Core

--- release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload_twl[986384];
+	extern unsigned char tgds_multiboot_payload_twl[989520];
 	extern int tgds_multiboot_payload_twl_size;
 #ifdef __cplusplus 
 } 
