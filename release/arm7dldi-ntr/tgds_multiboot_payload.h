28c5736fec66137244354692e5f4b52e657891bd
Coto <programmer.coto.1988@gmail.com>
2021-07-19 00:46:12 +0100

    - generate stable ARM9 newlib functions

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[857296];
+	extern unsigned char tgds_multiboot_payload[873872];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
