53e547611fdebc076ee8696e294cec9a7791ed2b
Juan Pablo Neira Valenzuela <jneira@xms.cl>
2021-08-17 11:30:03 -0400

    fix TWL arm9 binary

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[874932];
+	extern unsigned char tgds_multiboot_payload[875508];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
