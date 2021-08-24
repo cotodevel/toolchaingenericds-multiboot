c7bbf7f195dcdb99828fdc66852429bba119f704
Coto <programmer.coto.1988@gmail.com>
2021-08-24 03:28:34 +0100

    (from TGDS commit) -fix FIFO IRQs and SD reads (DLDI/TWL) use it

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[875880];
+	extern unsigned char tgds_multiboot_payload[876384];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
--- release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload_twl[893636];
+	extern unsigned char tgds_multiboot_payload_twl[893628];
 	extern int tgds_multiboot_payload_twl_size;
 #ifdef __cplusplus 
 } 
