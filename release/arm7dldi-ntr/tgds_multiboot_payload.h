332aa2abf5c06f7923be03a85311a58eb46dbf3e
Coto <programmer.coto.1988@gmail.com>
2021-08-24 05:16:59 +0100

    - move to ITCM some fatfs calls / enable faster seek

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[876384];
+	extern unsigned char tgds_multiboot_payload[876836];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
