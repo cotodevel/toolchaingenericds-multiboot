2bddd44617027121619b0fb33a1bfb1ee77a07ba
Coto <programmer.coto.1988@gmail.com>
2021-08-27 02:47:49 +0100

    - move to uncached scratchPad buffer and remove coherent cache calls (improves SD/DLDI stability)

--- release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload_twl[895096];
+	extern unsigned char tgds_multiboot_payload_twl[892952];
 	extern int tgds_multiboot_payload_twl_size;
 #ifdef __cplusplus 
 } 
