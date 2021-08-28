d3693195ba2b6120fec557f1238b934c6cae8625
Coto <programmer.coto.1988@gmail.com>
2021-08-28 06:29:41 +0100

    - ARM7 DLDI is used always for DLDI read/writes, ARM9 DLDI deprecated.

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[876732];
+	extern unsigned char tgds_multiboot_payload[874488];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
--- release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload_twl[892952];
+	extern unsigned char tgds_multiboot_payload_twl[891156];
 	extern int tgds_multiboot_payload_twl_size;
 #ifdef __cplusplus 
 } 
