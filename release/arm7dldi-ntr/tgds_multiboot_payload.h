2415025edb33a0f086f15f8962242dd091748799
Coto <programmer.coto.1988@gmail.com>
2021-08-29 21:20:35 +0100

    -downgrade armv5te ima-adpcm decoder to armv4t

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[874552];
+	extern unsigned char tgds_multiboot_payload[874680];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
--- release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload_twl[891220];
+	extern unsigned char tgds_multiboot_payload_twl[891348];
 	extern int tgds_multiboot_payload_twl_size;
 #ifdef __cplusplus 
 } 
