497a62848bffca5d5126f38f792a62c88c42e933
Coto <programmer.coto.1988@gmail.com>
2021-09-15 22:58:29 +0100

    - separate hardware layer into libhardware, which compiles through GCC enabling further optimizations.

--- release/arm7dldi-ntr/tgds_multiboot_payload.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload[953756];
+	extern unsigned char tgds_multiboot_payload[969288];
 	extern int tgds_multiboot_payload_size;
 #ifdef __cplusplus 
 } 
--- release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
+++ release/arm7dldi-ntr/tgds_multiboot_payload_twl.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
-	extern unsigned char tgds_multiboot_payload_twl[964792];
+	extern unsigned char tgds_multiboot_payload_twl[986916];
 	extern int tgds_multiboot_payload_twl_size;
 #ifdef __cplusplus 
 } 
