a858251f402e91060c364a731dcdf7c314991250
cotodevel <programmer.coto.1988@gmail.com>
2024-02-12 03:42:54 -0300

    Fix TGDS-Multiboot booting Slot 1 Passme v1 & v2 homebrew on both NTR & TWL mode.
    Slot 1 Passme v3 homebrew is unsupported on both NTR & TWL mode. It's got to do with custom MPU setup and other bootstub initialization not available on target Slot 1 Passme v3 binary, but instead, must be prepared by the loader before launching it.
    
    Note: TGDS-Multiboot running NTR homebrew (Slot 1 Passme v1 & v2) in TWL mode will break compatibility, as usual, because NTR binaries aren't 100% compatible in TWL mode by design, not my fault!

--- tgds_multiboot_payload/include/main.h
+++ tgds_multiboot_payload/include/main.h
@@ -38,7 +38,7 @@ extern int main(int argc, char **argv);
 extern char args[8][MAX_TGDSFILENAME_LENGTH];
 extern char *argvs[8];
 extern u8 argvIntraTGDSMB[256];
-extern void reloadARM7PlayerPayload(u32 arm7entryaddress, int arm7BootCodeSize);
+extern void reloadARM7Payload(u32 arm7entryaddress, int arm7BootCodeSize);
 extern char * getPayloadName();
 
 #ifdef __cplusplus
