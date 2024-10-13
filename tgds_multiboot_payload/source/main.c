523e3b224f286be844b674ae62da948983c717a9
cotodevel <programmer.coto.1988@gmail.com>
2024-10-12 23:43:41 -0300

    -ToolchainGenericDS-multiboot v3 is fixed & stable on NTR/TWL hardware as of 12 October of 2024

--- tgds_multiboot_payload/source/main.c
+++ tgds_multiboot_payload/source/main.c
@@ -72,6 +72,121 @@ __attribute__((optimize("Os")))
 #if (!defined(__GNUC__) && defined(__clang__))
 __attribute__ ((optnone))
 #endif
+void IRQInitCustom(u8 DSHardware)  {
+
+	//Disable mpu
+	CP15ControlRegisterDisable(CR_M);
+	
+	//Hardware ARM9 Init
+	//resetMemory_ARMCores(DSHardware); //no. Breaks TGDS-MB ARM7 bootldr core
+	
+
+	/////////////////////IRQ INIT
+	#ifdef ARM9
+	DrainWriteBuffer();
+	setTGDSARM9PrintfCallback((printfARM9LibUtils_fn)&TGDSDefaultPrintf2DConsole); //Setup default TGDS Project: console 2D printf 
+	#endif
+	
+	//FIFO IRQ Init
+	REG_IE = 0;
+	REG_IF = ~0;
+	REG_IME = 0;
+	
+	//FIFO IRQ Init
+	REG_IPC_SYNC = (1 << 14);	//14    R/W  Enable IRQ from remote CPU  (0=Disable, 1=Enable)
+	REG_IPC_FIFO_CR = IPC_FIFO_SEND_CLEAR | RECV_FIFO_IPC_IRQ  | FIFO_IPC_ENABLE;
+	
+	//Set up PPU IRQ: HBLANK/VBLANK/VCOUNT
+	#ifdef ARM7
+	REG_DISPSTAT = (DISP_VBLANK_IRQ | DISP_YTRIGGER_IRQ);
+	#endif
+	#ifdef ARM9
+	REG_DISPSTAT = (DISP_VBLANK_IRQ | DISP_YTRIGGER_IRQ);
+	#endif
+	
+	//Set up PPU IRQ Vertical Line
+	setVCountIRQLine(TGDS_VCOUNT_LINE_INTERRUPT);
+	
+	volatile uint32 interrupts_to_wait_armX = 0;	
+	#ifdef ARM7
+	interrupts_to_wait_armX = IRQ_TIMER1 | IRQ_VBLANK | IRQ_VCOUNT | IRQ_IPCSYNC | IRQ_RECVFIFO_NOT_EMPTY | IRQ_SCREENLID;
+	#endif
+	
+	#ifdef ARM9
+	interrupts_to_wait_armX = IRQ_VBLANK | IRQ_VCOUNT | IRQ_IPCSYNC | IRQ_RECVFIFO_NOT_EMPTY;
+	#endif
+	
+	REG_IE = interrupts_to_wait_armX;
+	INTERRUPT_VECTOR = (uint32)&NDS_IRQHandler;
+	
+	setupDisabledExceptionHandler();
+
+	int isNTRTWLBinary = isThisPayloadNTROrTWLMode();
+	if(isNTRTWLBinary == isTWLBinary){
+		__dsimode = true;
+		#ifdef ARM9
+		nocashMessage("TGDS:IRQInit():TWL Mode!");
+		#endif
+	}
+	else{
+		__dsimode = false;
+		#ifdef ARM9
+		nocashMessage("TGDS:IRQInit():NTR Mode!"); //ok so far
+		#endif
+	}
+	#ifdef TWLMODE
+		#ifdef ARM7
+		//TWL ARM7 IRQ Init
+		REG_AUXIE = 0;
+		REG_AUXIF = ~0;
+		irqEnableAUX(IRQ_I2C);
+		
+		//TGDS-Projects -> TWL TSC
+		TWLSetTouchscreenNTRMode();
+		#endif
+		
+		#ifdef ARM9
+		//TWL ARM9 IRQ Init
+		#endif
+	#endif
+
+	set0xFFFF0000FastMPUSettings(); //MPU needs to be setup here
+
+	REG_IME = 1;
+
+	////////////////////////////////IRQ INIT end
+	
+	//Enable mpu
+	CP15ControlRegisterEnable(CR_M);
+	
+	//Library init code
+	
+	//Newlib init
+	//Stream Devices Init: see devoptab_devices.c
+	//setbuf(stdin, NULL);
+	setbuf(stdout, NULL);	//iprintf directs to DS Framebuffer (printf already does that)
+	//setbuf(stderr, NULL);
+	
+	TryToDefragmentMemory();
+	
+	//Enable TSC
+	setTouchScreenEnabled(true);
+	
+	handleARM9InitSVC();
+	
+	savedDSHardware = (u32)DSHardware; //Global DS Firmware ARM7/ARM9
+	
+	//Shared ARM Cores
+	disableTGDSDebugging(); //Disable debugging by default
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
 int main(int argc, char **argv) {
 	
 	//Save ARGV-CMD line
@@ -80,12 +195,28 @@ int main(int argc, char **argv) {
 	
 	memset(0x02000000, 0, (int)512*1024); //clear RAM: Fixes DKARM / Custom Devkit NTR/TWL homebrew relying on RAM state not initialized properly
 	coherent_user_range_by_size((u32)0x02000000, (int)512*1024);
+
+	struct DLDI_INTERFACE * dldiCopy = (struct DLDI_INTERFACE *)&_io_dldi_stub;
+	char * dldiName = (char*)&dldiCopy->friendlyName[0];
+	bool isEMUEnv = ( (dldiName[0] == 'T') && (dldiName[1] == 'G') && (dldiName[2] == 'D') && (dldiName[3] == 'S') && (dldiName[4] == ' ')  );  
+
+	//ARM7DLDI = Slot1 or Slot2. VERY IMPORTANT, OTHERWISE THE ARM7 Core won't see the DLDI card mapped, even if the driver is there!!
+	if (dldiCopy->ioInterface.features & FEATURE_SLOT_GBA) {
+		SetBusSLOT1ARM9SLOT2ARM7();
+	}
+	if (dldiCopy->ioInterface.features & FEATURE_SLOT_NDS) {
+		SetBusSLOT1ARM7SLOT2ARM9();
+	}
+	
+	//Execute Stage 2: VRAM ARM7 payload: NTR/TWL (0x06000000). Required to have the DLDI context initializated in this payload.
+	executeARM7Payload((u32)0x02380000, 96*1024, TGDS_MB_V3_ARM7_STAGE1_ADDR);
 	
 	//TWL/NTR Mode bios will change here, so prevent jumps to BIOS exception vector through any interrupts
 	REG_IME = 0;
 	REG_IE = 0;
 	REG_IF = 0;
-	setupDisabledExceptionHandler();
+	CP15ControlRegisterDisable(CR_M); //prevent MPU irqs
+	
 	register int isNTRTWLBinary = (int)getValueSafe((u32*)ARM9_TWLORNTRPAYLOAD_MODE); //register means save this register and restore it everywhere it's used below. Save it now as it'll get erased
 	bool isTGDSTWLHomebrew = (bool)getValueSafe((u32*)TGDS_IS_TGDS_TWL_HOMEBREW); //is TGDS TWL homebrew? Uses special map
 	int tgds_multiboot_payload_size = (int)getValueSafe((u32*)TGDS_MB_V3_PAYLOAD_SIZE);
@@ -138,7 +269,20 @@ int main(int argc, char **argv) {
 		REG_EXMEMCNT = 0xE880;
 		initMBKARM9();
 	}
-	
+	else if(	//TGDS TWL ?
+		(__dsimode == true)
+		&&
+		(isNTRTWLBinary == isTWLBinary)
+		&&
+		(isTGDSTWLHomebrew == true)
+	){
+		//Enable 4M EWRAM (TWL)
+		u32 SFGEXT9 = *(u32*)0x04004008;
+		//14-15 Main Memory RAM Limit (0..1=4MB/DS, 2=16MB/DSi, 3=32MB/DSiDebugger)
+		SFGEXT9 = (SFGEXT9 & ~(0x3 << 14)) | (0x0 << 14);
+		*(u32*)0x04004008 = SFGEXT9;
+	}
+
 	//ARM9 SVCs & loader context initialized:
 	//Copy the file into non-case sensitive "tgdsboot.bin" into ARM7,
 	//since PetitFS only understands 8.3 DOS format filenames
@@ -169,26 +313,19 @@ int main(int argc, char **argv) {
 	u32 arm9iRamAddress = (u32)getValueSafe((u32*)ARM9i_RAM_ADDRESS);
 	int arm9iBootCodeSize = (int)getValueSafe((u32*)ARM9i_BOOT_SIZE);
 	
-	//Todo: Support isNDSBinaryV1Slot2 binary (Slot2 Passme v1 .ds.gba homebrew)
+	//Slot2 Passme v1 .ds.gba homebrew is unsupported. See: https://bitbucket.org/Coto88/toolchaingenericds-multiboot/issues/9/integrate-open-source-ntr-twl-bootloader
 	if(isNTRTWLBinary == isNDSBinaryV1Slot2){
 		u8 fwNo = *(u8*)ARM7_ARM9_SAVED_DSFIRMWARE;
 		int stage = 0;
 		handleDSInitError(stage, (u32)fwNo);	
 	}
 	
-	//TGDS TWL ?
-	if(
-		(__dsimode == true)
-		&&
-		(isNTRTWLBinary == isTWLBinary)
-		&&
-		(isTGDSTWLHomebrew == true)
-	){
-		//Enable 4M EWRAM (TWL)
-		u32 SFGEXT9 = *(u32*)0x04004008;
-		//14-15 Main Memory RAM Limit (0..1=4MB/DS, 2=16MB/DSi, 3=32MB/DSiDebugger)
-		SFGEXT9 = (SFGEXT9 & ~(0x3 << 14)) | (0x0 << 14);
-		*(u32*)0x04004008 = SFGEXT9;
+	//TGDS-MB v3 ARM7 bootldr error handling.
+	if((u32)arm9EntryAddress == (u32)0xFFFFFFFE){
+					handleDSInitOutputMessage("TGDS-MB: arm7bootldr/bootfile(): Boot fail. Notify developer.");
+			u8 fwNo = *(u8*)(0x027FF000 + 0x5D);
+			int stage = 10;
+			handleDSInitError(stage, (u32)fwNo);	
 	}
 	
 	coherent_user_range_by_size((uint32)arm7EntryAddress, arm7BootCodeSize); //Make ARM9 
@@ -202,9 +339,9 @@ int main(int argc, char **argv) {
 			coherent_user_range_by_size((uint32)arm9iRamAddress, arm9iBootCodeSize); //		   if we're launching one
 		}
 	}
-	else if(isNTRTWLBinary != isNDSBinaryV1Slot2){ //DLDI is for Slot 1 v1/v2/v3 NTR binaries only
+	if(isNTRTWLBinary != isNDSBinaryV1Slot2){ //DLDI is for Slot 1 v1/v2/v3 NTR / TGDS/DKARM hybrid TWL binaries only
 		u32 dldiSrc = (u32)&_io_dldi_stub;
-		bool stat = dldiPatchLoader((data_t *)arm9EntryAddress, (u32)arm9BootCodeSize, dldiSrc);
+		bool stat = dldiPatchLoader((data_t *)arm9EntryAddress, (int)arm9BootCodeSize, dldiSrc);
 		if(stat == true){
 			nocashMessage("DLDI patch success!");
 		}
