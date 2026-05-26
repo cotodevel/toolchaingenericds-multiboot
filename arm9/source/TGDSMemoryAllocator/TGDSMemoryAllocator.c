3de9b6f89d7638a68dcc9a0af76651d42f325f43
cotodevel <programmer.coto.1988@gmail.com>
2026-05-26 10:31:26 -0400

    -Fix TGDS NTR Payload undefined behavior in TWL mode: Assume disabled TWL hardware interaction.

--- arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
+++ arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
@@ -39,7 +39,7 @@ struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(bool isCustomT
 	customMemoryAllocator->customMalloc = isCustomTGDSMalloc;
 	
 	customMemoryAllocator->ARM9MallocStartaddress = (u32)sbrk(0);
-	customMemoryAllocator->memoryToAllocate = (1024*1024);
+	customMemoryAllocator->memoryToAllocate = (960*1024);
 	customMemoryAllocator->CustomTGDSMalloc9 = (TGDSARM9MallocHandler)&Xmalloc;
 	customMemoryAllocator->CustomTGDSCalloc9 = (TGDSARM9CallocHandler)&Xcalloc;
 	customMemoryAllocator->CustomTGDSFree9 = (TGDSARM9FreeHandler)&Xfree;
